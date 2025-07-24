e16c9edd06cb543ee15dee4209be1b322cde37e2
Mukkera Varshitha <157622436+Varshitha2324@users.noreply.github.com>
2025-07-24 18:32:28 +0530

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,14 @@
+public class Temperature{
+public static void main(String[]args){
+int temp=50;
+if(temp>45){
+System.out.println("Stay home");
+}
+else if(temp >=30 && temp<45){
+System.out.println("Carry Water Bottle");
+}
+else{
+System.out.println("can go normally");
+}
+}
+}
