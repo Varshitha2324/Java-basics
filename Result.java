7b4ff9f8d5211522378abdc862b2db0e6dcfe0ff
Mukkera Varshitha <157622436+Varshitha2324@users.noreply.github.com>
2025-07-24 18:43:08 +0530

    Add files via upload

---
+++ Result.java
@@ -0,0 +1,22 @@
+public class Result{
+public static void main(String[]args){
+int avgmarks=85;
+if(avgmarks>=90){
+System.out.println("Outstanding rank");
+}
+else if(avgmarks>=75){
+System.out.println("Distinction");
+}
+else if(avgmarks>=60){
+System.out.println("First Class");
+}
+else if(avgmarks>35){
+System.out.println("Pass");
+}
+else
+{
+System.out.println("Fail");
+}
+}
+}
+
